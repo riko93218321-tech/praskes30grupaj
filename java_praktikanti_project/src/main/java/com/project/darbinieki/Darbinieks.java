1bddacd2eb34679c01c38a806c8c5dcc64cd9b97
riko93218321-tech <riko.9321.8321@gmail.com>
2025-11-24 15:06:42 +0200

    Add files via upload

---
+++ java_praktikanti_project/src/main/java/com/project/darbinieki/Darbinieks.java
@@ -0,0 +1,2 @@
+package com.project.darbinieki;
+public class Darbinieks { int id; String vards; String uzvards; String amats; }
---
+++ java_praktikanti_project/src/main/java/com/project/uzdevumi/Uzdevums.java
@@ -0,0 +1,2 @@
+package com.project.uzdevumi;
+public class Uzdevums { int id; String nosaukums; String apraksts; String termins; String statuss; int darbinieksId; int materialsId; }
