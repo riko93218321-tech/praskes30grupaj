18955b2b7855f3330af1259672117cb6dd104739
riko93218321-tech <riko.9321.8321@gmail.com>
2025-11-26 14:35:03 +0200

    Delete java_praktikanti_project directory

--- java_praktikanti_project/src/main/java/com/project/darbinieki/Darbinieks.java
+++
@@ -1,2 +0,0 @@
-package com.project.darbinieki;
-public class Darbinieks { int id; String vards; String uzvards; String amats; }
--- java_praktikanti_project/src/main/java/com/project/uzdevumi/Uzdevums.java
+++
@@ -1,2 +0,0 @@
-package com.project.uzdevumi;
-public class Uzdevums { int id; String nosaukums; String apraksts; String termins; String statuss; int darbinieksId; int materialsId; }
